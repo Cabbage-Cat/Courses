5be495502b9e26e5257703bd5f7c48cf3f45cb81
SsorryQaQ <ssorryqaq@gmail.com>
2019-11-04 22:40:15 +0800

    ALG LAB 2DONE

--- Alg Lecture/Lab02/lab2.cpp
+++ Alg Lecture/Lab02/lab2.cpp
@@ -1,67 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 stack<int> num_stack;
-stack<char> e_stack;
+stack<char> sym_stack;
 map<char,int> pq;
 int main(){
-  pq['+'] = pq['-'] = 1;
-  pq['*'] = pq['/'] = 1;
-  pq['('] = 0;
+  pq['('] = 0; pq['+'] = pq['-'] = 1; pq['*'] = pq['/'] = 2; pq[':'] = 0;
   string line;
-  do{
-    cout << "Please input expr(q to exit):";
+  int quit_flag = 1;
+  while (quit_flag){
+    while (!num_stack.empty()) num_stack.pop();
+    while (!sym_stack.empty()) sym_stack.pop();
+    cout << "Please input expr:(q to quit):";
     cin >> line;
+    if (line=="q") { quit_flag = 0; continue; }
+    line.push_back(':'); // End symbol.
     int x = 0; int flag = 0;
-    for (char c : line){
-      if (c == '#') flag = 1;
-      else if (isdigit(c)) { x = x*10  + (c-'0'); }
+    for (int i=0;i<line.length();i++){
+      if (line[i] == '#') flag = 1;
+      else if (isdigit(line[i])){
+        x = x*10 + (line[i]-'0');
+        if (!isdigit(line[i+1])) { if (flag) { flag = 0; x = -x; } num_stack.push(x);  x = 0; }
+      }
       else{
-        if (!x) { if(flag) x=-x; num_stack.push(x); x = 0; }
-        if (c==')'){
-          while (e_stack.top() != '('){
-            char e = e_stack.top(); e_stack.pop();
+        char e = line[i];
+        if (e == '(') sym_stack.push(e);
+        else if (e == ')'){
+          while (sym_stack.top()!='('){
+            char tmpe = sym_stack.top(); sym_stack.pop();
             int x1 = num_stack.top(); num_stack.pop();
             int x2 = num_stack.top(); num_stack.pop();
-            switch (e)
-              {
+            switch (tmpe){
               case '+':
-                num_stack.push(x2+x1);
-                break;
-              case '-':
-                num_stack.push(x1-x2);
-                break;
-              case '*':
-                num_stack.push(x1*x2);
-                break;
-              case '/':
-                num_stack.push(x1/x2);
-                break;
-              }
-          }
-          e_stack.pop();
-        }
-        else if (e_stack.empty() || pq[e_stack.top()] <= pq[c]) e_stack.push(c);
-        else{
-          while (pq.empty() || pq[e_stack.top()] > pq[c]){
-            char e = e_stack.top(); e_stack.pop();
-            int x1 = num_stack.top(); num_stack.pop();
-            int x2 = num_stack.top(); num_stack.pop();
-            switch (e)
-            {
-            case '+':
               num_stack.push(x2+x1);
               break;
-            case '-':
-              num_stack.push(x1-x2);
+              case '-':
+              num_stack.push(x2-x1);
+              break;
+              case '*':
+              num_stack.push(x2*x1);
+              break;
+              case '/':
+              num_stack.push(x2/x1);
               break;
             }
           }
+          sym_stack.pop();
+        }
+        else if (sym_stack.empty() || sym_stack.top()=='(' || pq[sym_stack.top()] < pq[e]) sym_stack.push(e);
+        else{
+          while (!sym_stack.empty() && pq[sym_stack.top()] >= pq[e]) {
+            char tmpe = sym_stack.top(); sym_stack.pop();
+            int x1 = num_stack.top(); num_stack.pop();
+            int x2 = num_stack.top(); num_stack.pop();
+            switch (tmpe){
+              case '+':
+              num_stack.push(x1+x2);
+              break;
+              case '-':
+              num_stack.push(x1-x2);
+              break;
+              case '*':
+              num_stack.push(x1*x2);
+              break;
+              case '/':
+              num_stack.push(x1/x2);
+              break;
+            }
+          }
+          sym_stack.push(e);
         }
       }
     }
-    while (!num_stack.empty()) { cout << num_stack.top();}
-  }while (line!="q");
-
-  
+      cout << num_stack.top();
+  }
   return 0;
 }
