a2670a86c68ca35e23610f9bf98d1ca36603ab6d
SsorryQaQ <ssorryqaq@gmail.com>
2019-11-11 20:56:55 +0800

    ALG lab2 doc

--- Alg Lecture/Lab02/lab2.cpp
+++ Alg Lecture/Lab02/lab2.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
+#include <exception>
 using namespace std;
 stack<int> num_stack;
 stack<char> sym_stack;
 map<char,int> pq;
+
+struct no_expr_exception : public exception{
+  const char * what () const throw (){
+    return "Input is not a valid expression,please try again!\n";
+  }
+};
+
+struct divide_zero_exception : public exception{
+  const char *what() const throw(){
+    return "Expression has divide 0,please try again!\n";
+  }
+};
+
 int main(){
   pq['('] = 0; pq['+'] = pq['-'] = 1; pq['*'] = pq['/'] = 2; pq[':'] = 0;
   string line;
   int quit_flag = 1;
-  while (quit_flag){
+  try{
+    while (quit_flag){
     while (!num_stack.empty()) num_stack.pop();
     while (!sym_stack.empty()) sym_stack.pop();
     cout << "Please input expr:(q to quit):";
@@ -26,6 +41,7 @@ int main(){
         if (e == '(') sym_stack.push(e);
         else if (e == ')'){
           while (sym_stack.top()!='('){
+            if (sym_stack.size()<2 || num_stack.size()<2) throw no_expr_exception();
             char tmpe = sym_stack.top(); sym_stack.pop();
             int x1 = num_stack.top(); num_stack.pop();
             int x2 = num_stack.top(); num_stack.pop();
@@ -49,9 +65,10 @@ int main(){
         else if (sym_stack.empty() || sym_stack.top()=='(' || pq[sym_stack.top()] < pq[e]) sym_stack.push(e);
         else{
           while (!sym_stack.empty() && pq[sym_stack.top()] >= pq[e]) {
+            if (sym_stack.size()<1 || num_stack.size()<2) throw no_expr_exception();
             char tmpe = sym_stack.top(); sym_stack.pop();
-            int x1 = num_stack.top(); num_stack.pop();
             int x2 = num_stack.top(); num_stack.pop();
+            int x1 = num_stack.top(); num_stack.pop();
             switch (tmpe){
               case '+':
               num_stack.push(x1+x2);
@@ -63,6 +80,7 @@ int main(){
               num_stack.push(x1*x2);
               break;
               case '/':
+              if (x2==0) throw divide_zero_exception();
               num_stack.push(x1/x2);
               break;
             }
@@ -71,7 +89,12 @@ int main(){
         }
       }
     }
-      cout << num_stack.top();
+      cout << num_stack.top()<<endl;
   }
+  }
+  catch(const std::exception& e){
+    std::cerr << e.what() << '\n';
+  }
+  
   return 0;
 }
